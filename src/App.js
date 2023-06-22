ce8aa14ce5c29b52d47044c273036fd91ccf59de
Vaibhav <103978586+vaibhav15git@users.noreply.github.com>
2023-06-22 16:55:09 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
+import { ToastContainer } from "react-toastify";
+import AllUsers from "./Components/AllUsers";
+import Update from "./Components/Update";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer theme="colored" className='text-dark'></ToastContainer>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/update/:id" element={<Update/>}></Route>
+          <Route path="/allusers" element={<AllUsers/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
